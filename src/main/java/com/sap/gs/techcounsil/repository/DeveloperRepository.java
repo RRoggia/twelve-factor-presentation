f995b0f712563a2bdec7027cd0f6c11cd90713c0
RRoggia <renanzr@gmail.com>
2019-04-25 16:20:08 -0300

    add crud repository for Developer

---
+++ src/main/java/com/sap/gs/techcounsil/repository/DeveloperRepository.java
@@ -0,0 +1,9 @@
+package com.sap.gs.techcounsil.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.sap.gs.techcounsil.model.Developer;
+
+public interface DeveloperRepository extends CrudRepository<Developer, Long> {
+
+}
